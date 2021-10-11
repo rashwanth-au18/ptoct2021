a32b0d04b023af9d8ae759523e0d66441bce7031
rashwanth-au18 <rashwanth.au18@bitsathy.ac.in>
2021-10-11 10:09:08 +0000

    third

--- java/add.java
+++ java/add.java
@@ -4,7 +4,15 @@ class add
 	{
 		int a=5,b=5,c;
 		c=a+b;
-		System.out.println(c);
+		int d=Integer.parseInt(args[0])+Integer.parseInt(args[1]);
+		float e=Float.parseFloat(args[2])+Float.parseFloat(args[3]);
+		System.out.println("constant:" + c);
+		System.out.println("Integer:" + (Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+		System.out.println("Float:" + (Float.parseFloat(args[2])+Float.parseFloat(args[3])));
+		 System.out.println(d);
+		 System.out.println(e);
+		 System.out.println(d+" "+e);
 	}
+
 }
 		
